025ac3eb79a6bf4ad10e93d87614dac2bce14c67
Fatih Eker <fatiheker97@gmail.com>
2025-05-28 16:27:38 +0300

    BeforeCustomFonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,88 @@ import Entry from "./Components/Entry/Entry.jsx";
 import Ads from "./Components/Ads/Ads.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import {categories} from "./data/data.js";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import OrderForm from "./Components/Form/OrderForm.jsx";
+import error from "eslint-plugin-react/lib/util/error.js";
+
+
+const initialFormData = {
+    pizzaSize: "",
+    dough: "",
+    ingredients: [],
+    orderNote: "",
+    quantity: 1,
+}
+
+const initialErrors = {
+    sizeEmpty: true,
+    doughEmpty: true,
+}
 
 function App() {
 
     const [page, setPage] = useState("home")
+    const [formData, setFormData] = useState(initialFormData);
+    const [errors, setErrors] = useState(initialErrors);
+    const [sendActive, setSendActive] = useState(false);
+
+
+    useEffect(() => {
+        errors.sizeEmpty = !formData.pizzaSize;
+        errors.doughEmpty = !formData.dough;
+        setSendActive(checkSendable());
+    }, [formData.pizzaSize, formData.dough]);
+
+    const checkSendable = () => {
+        return Object.values(errors).every(x => x === false)
+    }
 
     function Ttt() {
         page === "form" ? setPage("home") : setPage("form")
     }
+
+    function onChange(e) {
+        const {type, name, value, checked} = e.target;
+
+        setFormData(prev => {
+            if (name === "ingredients") {
+                const updated = checked ? [...prev.ingredients, value] : prev.ingredients.filter(item => item !== value);
+                return {...prev, ingredients: updated};
+            }
+
+            if (name === "quantity") {
+                const num = Number(value);
+
+                if (num < 1) {
+                    const confirmCancel = window.confirm("Siparişinizi iptal etmek ister misiniz?");
+                    if (confirmCancel) {
+                        setFormData(initialFormData);
+                        setPage("home");
+                    } else {
+
+                        setFormData(prev => ({...prev, [name]: num}));
+                        return;
+                    }
+                }
+            }
+
+            const val = type === "checkbox" ? checked : value;
+            return {...prev, [name]: val};
+        });
+    }
+
+    function handleSubmit(e) {
+        if (!checkSendable()) return;
+        console.log("x");
+    }
+
+    function homePage() {
+        setPage("home")
+    }
+
+    useEffect(() => {
+        console.log(formData)
+    }, [formData]);
 
     const PageSwap = () => {
         switch (page) {
@@ -23,7 +95,7 @@ function App() {
                     <Footer/>
                 </>
             case "form":
-                return <OrderForm/>
+                return <OrderForm onChange={onChange} formData={formData} homePage={homePage} sendActive={sendActive}/>
             default:
                 return 0;
         }
@@ -34,8 +106,7 @@ function App() {
     return (
         <>
             {PageSwap()}
-            {console.log(page)};
-            <button onClick={() =>Ttt()}>Form</button>
+            <button onClick={() => Ttt()}>Form</button>
         </>
     )
 }
--- src/Components/Form/OrderForm.jsx
+++ src/Components/Form/OrderForm.jsx
@@ -1,24 +1,29 @@
 ﻿import './OrderForm.css';
 
-function OrderForm() {
+
+function OrderForm({onChange, formData, homePage, sendActive}) {
+    const {pizzaSize, ingredients, quantity} = formData;
     return (
-        <section>
+        <section className="orderFormSection barlow">
             <div className="header">
-                <div className="image">
-                    <img src="images/iteration-1-images/logo.svg" alt=""/>
-                </div>
-                <div className="ugh">
-                    <div className="header-text">
-                        <span>Anasayfa-<strong>Sipariş Oluştur</strong></span>
-                    </div>
-                </div>
+                <img src="images/iteration-1-images/logo.svg" alt=""/>
             </div>
-            <div className="form-content">
-                <div className="form-container">
-                    <div className="pizza-text">
+            <div className="grayArea">
+                <div className="grayContent">
+                    <div className="image">
+                        <img src="images/iteration-2-images/pictures/form-banner.png" alt=""/>
+                    </div>
+
+                    <div className="ugh">
+                        <div className="header-text barlow">
+                            <a onClick={homePage}>Anasayfa-</a><strong>Sipariş Oluştur</strong>
+                        </div>
+                    </div>
+
+                    <div className="pizza-text barlow">
                         <h2>Position Absolute Acı Pizza</h2>
-                        <div className="price-data">
-                            <span><strong>Price</strong></span><span>Score</span><span>Rating</span>
+                        <div className="price-data barlow">
+                            <span><strong className="price">Price</strong></span><span>Score</span><span>Rating</span>
                         </div>
                         <p>
                             Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam
@@ -29,34 +34,49 @@ function OrderForm() {
                             bazen pizzetta denir.
                         </p>
                     </div>
+
+                </div>
+            </div>
+
+
+            <div className="form-content">
+                <div className="form-container">
+
                     <div className="pizza-size">
                         <div className="size">
                             <h2>Boyut Seç</h2>
                             <div>
-                                <input type="radio" name="size" id="small"/>
+                                <input checked={pizzaSize === "Küçük Boy"} onChange={onChange} value="Küçük Boy"
+                                       type="radio"
+                                       name="pizzaSize" id="small"/>
                                 <label htmlFor="small">Küçük</label>
                             </div>
                             <div>
-                                <input type="radio" name="size" id="mid"/>
+                                <input checked={pizzaSize === "Orta Boy"} onChange={onChange} value="Orta Boy"
+                                       type="radio"
+                                       name="pizzaSize" id="mid"/>
                                 <label htmlFor="mid">Orta</label>
                             </div>
                             <div>
-                                <input type="radio" name="size" id="big"/>
+                                <input checked={pizzaSize === "Büyük Boy"} onChange={onChange} value="Büyük Boy"
+                                       type="radio"
+                                       name="pizzaSize" id="big"/>
                                 <label htmlFor="big">Büyük</label>
                             </div>
 
                         </div>
-                        <div className="dough">
+                        <div className="hamurSecim">
                             <div>
                                 <h2>Hamur Seç</h2>
                                 <select
-                                    id="hamurSecim"
+                                    onChange={onChange}
+                                    name="dough"
                                     aria-label="Lütfen hamur seçiminizi yapın"
                                     defaultValue=""
                                 >
                                     <option value="" disabled>Hamur Kalınlığı</option>
-                                    <option value="ince">İnce</option>
-                                    <option value="normal">Normal</option>
+                                    <option value="İnce">İnce</option>
+                                    <option value="Normal">Normal</option>
                                     <option value="kalın">Kalın</option>
                                 </select>
 
@@ -65,80 +85,202 @@ function OrderForm() {
                     </div>
                     <h2>Ek Malzemeler</h2>
                     <span>En fazla 10 malzeme seçebilirsiniz. 5₺</span>
-                    <div className="checkboxes">
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Pepperoni"/>
-                            <label htmlFor="Pepperoni">Pepperoni</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Sosis"/>
-                            <label htmlFor="Sosis">Sosis</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="KanadaJambonu"/>
-                            <label htmlFor="KanadaJambonu">Kanada Jambonu</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="TavukIzgara"/>
-                            <label htmlFor="TavukIzgara">Tavuk Izgara</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Soğan"/>
-                            <label htmlFor="Soğan">Soğan</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Domates"/>
-                            <label htmlFor="Domates">Domates</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Mısır"/>
-                            <label htmlFor="Mısır">Mısır</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Sucuk"/>
-                            <label htmlFor="Sucuk">Sucuk</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Jalepeno"/>
-                            <label htmlFor="Jalepeno">Jalepeno</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Sarımsak"/>
-                            <label htmlFor="Sarımsak">Sarımsak</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Biber"/>
-                            <label htmlFor="Biber">Biber</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Sucuk2"/>
-                            <label htmlFor="Sucuk2">Başka Sucuk</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Ananas"/>
-                            <label htmlFor="Ananas">Ananas</label>
-                        </div>
-                        <div className="checkbox">
-                            <input type="checkbox" value="" id="Kabak"/>
-                            <label htmlFor="Kabak">Kabak</label>
-                        </div>
 
+
+                    <div className="checkboxes">
+
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Pepperoni"*/}
+                        {/*            checked={ingredients.includes("Pepperoni")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Pepperoni">Pepperoni</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Sosis"*/}
+                        {/*            checked={ingredients.includes("Sosis")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Sosis">Sosis</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Kanada Jambonu"*/}
+                        {/*            checked={ingredients.includes("Kanada Jambonu")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="KanadaJambonu">Kanada Jambonu</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Tavuk Izgara"*/}
+                        {/*            checked={ingredients.includes("Tavuk Izgara")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="TavukIzgara">Tavuk Izgara</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Soğan"*/}
+                        {/*            checked={ingredients.includes("Soğan")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Soğan">Soğan</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Domates"*/}
+                        {/*            checked={ingredients.includes("Domates")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Domates">Domates</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Mısır"*/}
+                        {/*            checked={ingredients.includes("Mısır")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Mısır">Mısır</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Sucuk"*/}
+                        {/*            checked={ingredients.includes("Sucuk")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Sucuk">Sucuk</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Jalepeno"*/}
+                        {/*            checked={ingredients.includes("Jalepeno")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Jalepeno">Jalepeno</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Sarımsak"*/}
+                        {/*            checked={ingredients.includes("Sarımsak")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Sarımsak">Sarımsak</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Biber"*/}
+                        {/*            checked={ingredients.includes("Biber")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Biber">Biber</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Sucuk2"*/}
+                        {/*            checked={ingredients.includes("Sucuk2")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Sucuk2">Başka Sucuk</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            onChange={onChange}*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Ananas"*/}
+                        {/*            checked={ingredients.includes("Ananas")}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Ananas">Ananas</label>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="checkbox">*/}
+                        {/*        <input*/}
+                        {/*            type="checkbox"*/}
+                        {/*            name="ingredients"*/}
+                        {/*            value="Kabak"*/}
+                        {/*            checked={formData.ingredients.includes("Kabak")}*/}
+                        {/*            onChange={onChange}*/}
+                        {/*        />*/}
+                        {/*        <label htmlFor="Kabak">Kabak</label>*/}
+                        {/*    </div>*/}
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/> <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+
+
+                        <div className="checkbox">
+                            <input type="checkbox"
+                                   name="ingredients"
+                                   id="Kabak"
+                                   value="Kabak"
+                                   checked={formData.ingredients.includes("Kabak")}
+                                   onChange={onChange}
+                            />
+                            <label htmlFor="Kabak" className="checkLabel"></label>
+                        </div>
                     </div>
+
+
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+
+
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+
+
                     <div className="order-note">
                         <h2>Sipariş Notu</h2>
-                        <textarea name="" id="" rows="3" placeholder={"Siparişine eklemek istediğin bir not var mı?"}>
+                        <textarea onChange={onChange} name="orderNote" id="orderNote" rows="3"
+                                  placeholder={"Siparişine eklemek istediğin bir not var mı?"}>
 
                         </textarea>
                     </div>
                     <hr/>
                     <div className="summary">
-                        <input className="number" type="number"/>
+                        <input onChange={onChange} value={quantity} name="quantity" className="number" type="number"/>
                         <div className="summary-price">
                             <h2>Sipariş Toplamı</h2>
                             <div>
                                 <p>Seçimler<span> 25.00₺</span></p>
                                 <p>Toplam <span>110.00₺</span></p>
                             </div>
+                            <button disabled={!sendActive}>Sipariş ver</button>
                         </div>
                     </div>
                 </div>
