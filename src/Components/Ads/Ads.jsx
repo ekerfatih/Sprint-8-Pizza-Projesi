72724fec9c863966c117138af3862875023f2750
Fatih Eker <fatiheker97@gmail.com>
2025-06-26 18:53:12 +0300

    s

--- src/Components/Ads/Ads.jsx
+++ src/Components/Ads/Ads.jsx
@@ -5,7 +5,7 @@ import Billboard from "./Billboard.jsx";
 function Ads({categories,openForm}) {
     return (
         <section className="ads-section">
-            <Billboard openForm={openForm}/>
+            <Billboard />
             <Selects categories={categories}/>
             <Products/>
         </section>
